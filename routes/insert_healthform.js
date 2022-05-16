4857027fc4ca1c79889aeefbaaad7ad1bbf335f0
unknown <icep.memory1@gmail.com>
2022-05-16 13:19:42 +0200

    health form correction

--- routes/insert_healthform.js
+++ routes/insert_healthform.js
@@ -19,6 +19,7 @@ router.post('/user',(req,res)=>{
     //console.log(req.body,'formData');
 
     //instatiating user variables
+  
 
   let User_id=req.body.User_id;
   let vstatus =req.body.vstatus;
@@ -40,76 +41,147 @@ router.post('/user',(req,res)=>{
   //Hamilton
 //sending the variables to the database
 
-if(vstatus==="No" && covid19==="No" && recentCough==="No" && difficultyBreathing==="No" && weight_loss==="No" && heavyHeadache==="No" && infectedPerson==="No"){
-status="GOOD";
-Form_check="Yes" ; 
-}
-if(vstatus==="No" && covid19==="Yes" && recentCough==="No" && difficultyBreathing==="No"  && weight_loss==="No" && heavyHeadache==="No" && infectedPerson==="No"){
-    status="DANGEROUS";
-    Form_check="Yes" ;   
-    }
-    if(vstatus==="Yes" && covid19==="No" || recentCough==="Yes" || difficultyBreathing==="Yes"|| weight_loss==="No"  || heavyHeadache==="Yes" || infectedPerson==="Yes"){
-        status="WARNING";
-        Form_check="Yes" ;    
-        }
 
- 
 
-//let qr=`insert into healthForm(User_id,Vaccination_status,Covid19,Recent_cough,Difficulty_breath,Weight_lose,Headache,Infected_person,Form_check,Status) values('${User_id}','${vstatus}','${covid19}','${recentCough}','${difficultyBreathing}','${weight_loss}','${heavyHeadache}','${infectedPerson}','${Form_check}','${status}')`;
-let qr=`insert into record(User_id,Form_check,Status,Date) values('${User_id}','${Form_check}','${status}','${formateDate}')`
-database.query(qr,(err,result)=>{
-
-    if(err){console.log(err);
+let qC = `select * from record where User_id ='${User_id}'and Date='${date.toDateString()}'  `;
     
-        res.send({message:'data Not inserted'});
-    }else{
+    database.query(qC,(err,result)=>{
+    
+    if(err){console.log(err);}
+    
+    if(result.length>0)
+       {
         res.send({
-            message:'data inserted'
-			
-        });               
+            message:'you already filled the FORM',
+            data:result
+           
+        });
+        console.log(result);
+       }
+       else 
+       {
+           
+
+if(vstatus==="No" && covid19==="No" && recentCough==="No" && difficultyBreathing==="No" && weight_loss==="No" && heavyHeadache==="No" && infectedPerson==="No"){
+    status="GOOD";
+    Form_check="Yes" ; 
     }
-   //console.log(result,'result')
-console.log(formateDate)
-       
+    else if(vstatus==="No" && covid19==="Yes" && recentCough==="No" && difficultyBreathing==="No"  && weight_loss==="No" && heavyHeadache==="No" && infectedPerson==="No"){
+        status="DANGEROUS";
+        Form_check="Yes" ;   
+        }
+       else if(vstatus==="No" && covid19==="No" || recentCough==="Yes" || difficultyBreathing==="Yes"|| weight_loss==="No"  || heavyHeadache==="Yes" || infectedPerson==="Yes"){
+            status="WARNING";
+            Form_check="Yes" ;    
+            }
+    
+     
+    
+    let qr=`insert into healthForm(User_id,Vaccination_status,Covid19,Recent_cough,Difficulty_breath,Weight_lose,Headache,Infected_person,Form_check,Status) values('${User_id}','${vstatus}','${covid19}','${recentCough}','${difficultyBreathing}','${weight_loss}','${heavyHeadache}','${infectedPerson}','${Form_check}','${status}')`;
+    //let qr=`insert into record(User_id,Form_check,Status,Date) values('${User_id}','${Form_check}','${status}','${formateDate}')`
+    database.query(qr,(err,result)=>{
+    
+        if(err){console.log(err);
         
+            res.send({message:'data Not inserted'});
+        }else{
+            res.send({
+                message:'data inserted'
+                
+            });               
+        }
+       //console.log(result,'result')
+    console.log(formateDate)
+         
+    
+            
+            const render = res.render;
+        const send = res.send;
+        res.render = function renderWrapper(...args) {
+            Error.captureStackTrace(this);
+            return render.apply(this, args);
+    
+        };
+    
+        res.send = function sendWrapper(...args) {
+            try {
+                send.apply(this, args);
+            } catch (err) {
+                console.error(`Error in res.send | ${err.code} | ${err.message} | ${res.stack}`);
+            }
+        };
+    
+    
+    
+    
+    
+    });
+    //inserting to the second table
+    
+    let qRecord=`insert into record(User_id,Form_check,Status,Date) values('${User_id}','${Form_check}','${status}','${formateDate}')`;
+    database.query(qRecord,(err,result)=>{
+    
+        if(err){console.log(err);
+        
+            res.send({message:'data Not inserted'});
+        }else{
+            res.send({
+                message:'data inserted'
+                
+            });               
+        }
+       //console.log(result,'result')
+    console.log(formateDate)
+         
+    
+            
+            const render = res.render;
+        const send = res.send;
+        res.render = function renderWrapper(...args) {
+            Error.captureStackTrace(this);
+            return render.apply(this, args);
+    
+        };
+    
+        res.send = function sendWrapper(...args) {
+            try {
+                send.apply(this, args);
+            } catch (err) {
+                console.error(`Error in res.send | ${err.code} | ${err.message} | ${res.stack}`);
+            }
+        };
+    
+    
+    
+    
+    
+    });
+    
+        
+      }
+    
+
+
+      const render = res.render;
+      const send = res.send;
+      res.render = function renderWrapper(...args) {
+          Error.captureStackTrace(this);
+          return render.apply(this, args);
+  
+      };
+  
+      res.send = function sendWrapper(...args) {
+          try {
+              send.apply(this, args);
+          } catch (err) {
+              console.error(`Error in res.send | ${err.code} | ${err.message} | ${res.stack}`);
+          }
+      };
   
 
-
-        const render = res.render;
-    const send = res.send;
-    res.render = function renderWrapper(...args) {
-        Error.captureStackTrace(this);
-        return render.apply(this, args);
-
-    };
-
-    res.send = function sendWrapper(...args) {
-        try {
-            send.apply(this, args);
-        } catch (err) {
-            console.error(`Error in res.send | ${err.code} | ${err.message} | ${res.stack}`);
-        }
-    };
+    });
 
 
-
-});
-
-
-
-    //next();
-	
-/* 	let qr_record=`insert into record(User_id,Form_check,Status) values('${User_id}','${Form_check}','${status}')`;
-
-database.query(qr_record,(err,result)=>{
-
-    if(err){console.log(err);
-    
-        res.send({message:'data Not inserted'});
-    }else{
-        res.send({
-            message:'data inserted'
-        });               
-    } */
+///end of function
 
 });
