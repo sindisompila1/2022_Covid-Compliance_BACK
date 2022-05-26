9b0f24c74864953afb6b4cc9da362396e73befab
Sindiso Mpila <37083365+sindisompila1@users.noreply.github.com>
2022-05-26 09:56:31 +0200

    Update two_items.js

--- routes/two_items.js
+++ routes/two_items.js
@@ -9,28 +9,38 @@ const database=require('./database');
 
 router.get('/record/:User_id',(req,res)=>{
     let gID = req.params.User_id;
+    let date = new Date();
     
-    let qr = `select Form_check from record where User_id = '${gID}'`;
+    let qr = `select Form_check from record where Form_check='Yes' and  Date ='${date.toDateString()}' and  User_id = '${gID }' `;
+    
     
     database.query(qr,(err,result)=>{
     
-    if(err){console.log(err);}
-    
-    if(result.length>0)
-       {
-        res.send({
-            message:'Successful',
-            data:result
-            
-        });
-       }
-       else 
-       {
+      if(err){console.log(err);
+        console.log(result,'result')
+        res.send({message:'Unsuccessful'});
+        } 
+        else{
+            if(result.length>0)
+            {
             res.send({
-             message:'Unsuccessful'
-        })
-      }
-    
-    });
+                message:'Successful',
+                data:result
+            });
+          }
+          else{
+
+            res.send({
+                message:'Unsuccessful',
+                data:result
+            });
+          }
+        }
+
+            
+    }); 
        
-})
+      
+ 
+
+  });
